d3d9c6a1973eceba519038f396556cf7e4566d8b
andreasgranqvist-teamit <andreas.granqvist@teamit.fi>
2025-01-23 09:45:37 +0200

    Small fixes and change poetry to uv (#2)
    * latest improvements, mainly UV
    ---------
    
    Co-authored-by: Andreas Granqvist <andreasgranqvist@gmail.com>

--- task_2.py
+++ task_2.py
@@ -1,14 +1,14 @@
 """Task 2: SQL
 
-2. In the following cell, calculate the ordinal number of each bank failure per State based on the chronological order of failure.  
+2. Calculate the ordinal number of each bank failure per State based on the chronological order of failure.  
 The result should show the Bank, city, State and the order in which it failed compared to its peers.  
 Sort the results according to the State, Bank and Date.
  Hint: Window functions
-"""
+ """
 
 import duckdb
 
-bank_failures_data = duckdb.read_parquet("bank_failures.parquet").show()
+bank_failures_data = duckdb.read_parquet("data/bank_failures.parquet").show()
 
 # duckdb.sql("""
 # Write your code here!
